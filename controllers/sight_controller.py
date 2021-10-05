a6cd7e24a54ca2fd9ae8de461d59261b46b1ffc6
rm -rf .git <jlparson@hotmail.com>
2021-10-05 10:45:50 +0100

    remove country controller

--- controllers/sight_controller.py
+++ controllers/sight_controller.py
@@ -12,3 +12,62 @@ sights_blueprint = Blueprint("sights", __name__)
 def sights():
     sights = sight_repository.select_all()
     return render_template("sights/index.html", all_sights = sights)
+
+
+# SIGHTS VISITED
+@sights_blueprint.route("/sights/visited")
+def sights_visited():
+    sights = sight_repository.select_all()
+    return render_template("sights/visited.html", sights=sights)
+
+# SIGHTS BUCKET LIST
+@sights_blueprint.route("/sights/bucketlist")
+def sights_bucket_list():
+    sights = sight_repository.select_all()
+    return render_template("sights/bucketlist.html", sights=sights)
+
+
+# NEW
+# GET '/sights/new'
+@sights_blueprint.route("/sights/new", methods=['GET'])
+def new_sight():
+    cities = city_repository.select_all()
+    return render_template("sights/new.html", all_cities=cities)
+
+# CREATE
+# POST '/sights'
+@sights_blueprint.route("/sights", methods=['POST'])
+def create_sight():
+    name = request.form['name']
+    city = city_repository.select(request.form['city_id'])
+    visited = request.form["visited"]
+    sight = Sight(name, city, visited)
+    sight_repository.save(sight)
+    return redirect('/sights')
+
+# EDIT
+@sights_blueprint.route("/sights/<id>/edit", methods=['GET'])
+def edit_sight(id):
+    sights = sight_repository.select(id)
+    cities = city_repository.select_all()
+    return render_template('/sights/edit.html', cities=cities, sights=sights)
+
+# UPDATE
+@sights_blueprint.route("/sights/<id>", methods=['POST'])
+def update_sight(id):
+    name = request.form['name']
+    city = city_repository.select(request.form['city_id'])
+    visited = request.form["visited"]
+    sight = Sight(name, city, visited)
+    print(sight.city.name)
+    sight_repository.update(sight)
+    return redirect('/sights')
+
+# DELETE
+@sights_blueprint.route("/sights/<id>/delete", methods=['POST'])
+def delete_sight(id):
+    sight_repository.delete(id)
+    return redirect('/sights/')
+
+
+
